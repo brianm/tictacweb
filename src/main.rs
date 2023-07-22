0f192e718a3ddb35133d45ca678977f1d3a8a494
Brian McCallister <brianm@skife.org>
2023-07-22 10:51:02 -0700

    add a sample test

--- src/main.rs
+++ src/main.rs
@@ -61,3 +61,18 @@ struct World {
 fn index() -> &'static str {
     "Hello, world!"
 }
+
+#[cfg(test)]
+mod test {
+    use super::rocket;
+    use rocket::local::blocking::Client;
+    use rocket::http::Status;
+
+    #[test]
+    fn hello_world() {
+        let client = Client::tracked(rocket()).expect("valid rocket instance");
+        let response = client.get(uri!(super::index)).dispatch();
+        assert_eq!(response.status(), Status::Ok);
+        assert_eq!(response.into_string().unwrap(), "Hello, world!");
+    }
+}

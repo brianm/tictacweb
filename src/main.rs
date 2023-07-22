91c655d08d650875a81934581431b60dfa9788cc
Brian McCallister <brianm@skife.org>
2023-07-22 08:57:00 -0700

    start modeling board

--- src/main.rs
+++ src/main.rs
@@ -4,51 +4,60 @@ extern crate rocket;
 #[macro_use]
 extern crate serde;
 
-use rocket::response::Redirect;
-use rocket::State;
 use dashmap::DashMap;
 use rocket::response::status::NotFound;
+use rocket::response::Redirect;
+use rocket::serde::{json::Json, Serialize};
+use rocket::State;
 use ulid::Ulid;
 
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+#[post("/game")]
+fn new_game(state: &State<World>) -> Redirect {
+    let game_id = Ulid::new().to_string();
+    state.games.insert(
+        game_id.clone(),
+        GameState::NotStarted {
+            board: Board([[' '; 3]; 3]),
+        },
+    );
+    Redirect::to(uri!(game(game_id = game_id)))
 }
 
 #[get("/game/<game_id>")]
-fn game(state: &State<World>, game_id: &str) -> Result<String, NotFound<String>> {
+fn game(state: &State<World>, game_id: &str) -> Result<Json<GameState>, NotFound<String>> {
     let go = state.games.get(game_id);
     match go {
-        Some(game) => return Ok(format!("{:?}", game.value())),
+        Some(game) => return Ok(Json::from(game.value().clone())),
         None => return Err(NotFound(format!("Game {} not found", game_id))),
     };
-}
-
-#[post("/new")]
-fn new_game(state: &State<World>) -> Redirect {
-    let game_id = Ulid::new().to_string();
-    state.games.insert(game_id.clone(), GameState::NotStarted);
-    Redirect::to(uri!(game(game_id = game_id)))
 }
 
 #[launch]
 fn rocket() -> _ {
     rocket::build()
-    .manage(World { 
-        games: DashMap::new()
-    })
-    .mount("/", routes![index, new_game, game])
+        .manage(World {
+            games: DashMap::new(),
+        })
+        .mount("/", routes![index, new_game, game])
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, Copy, Clone)]
 enum GameState {
-    NotStarted,
-    XTurn,
-    YTurn,
-    XWon,
-    YWon,
+    NotStarted { board: Board },
+    XTurn { board: Board },
+    YTurn { board: Board },
+    XWon { board: Board },
+    YWon { board: Board },
 }
+
+#[derive(Debug, Serialize, Deserialize, Copy, Clone)]
+struct Board([[char; 3]; 3]);
 
 struct World {
     games: DashMap<String, GameState>,
+}
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
 }
